288926ecdfee0f2c208d5f4e168fb8b85e5ecb0d
Darrell Wright <beached@users.noreply.github.com>
2023-02-01 09:16:01 -0500

    small name cleanup

--- src/daw/thunk.cpp
+++ src/daw/thunk.cpp
@@ -3,7 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-// Official repository: https://github.com/beached/daw_thunk_a_dunk
+// Official repository: https://github.com/beached/daw_thunk
 //
 
 #include "daw/daw_erased_callable.h"
